1548b9caaa6cd36fac2ab3f666e77b624caccad1
ejjonny <ejjonny@gmail.com>
2024-09-10 23:06:42 -0600

    revert demo changes

--- examples/egui-example/src/main.rs
+++ examples/egui-example/src/main.rs
@@ -21,19 +21,21 @@ fn main() -> eframe::Result {
 }
 
 fn my_layout_fn(ui: &mut Ui) -> Node<Ui> {
-    column(vec![
-        row(vec![
-            // draw_a(ui).height(20.),
-            draw_b(ui).height(10.),
-            draw_c(ui).height(30.),
-        ]),
-        row(vec![
-            // draw_a(ui).height(20.),
-            draw_b(ui),
-            draw_c(ui).height(30.),
-        ])
-        .pad(0.),
-    ])
+    column_spaced(
+        10.,
+        vec![
+            draw_a(ui),
+            row_spaced(
+                10.,
+                vec![
+                    draw_b(ui).width_range(200.0..),
+                    column_spaced(10., vec![draw_a(ui), draw_b(ui), draw_c(ui)]),
+                ],
+            ),
+            draw_c(ui),
+        ],
+    )
+    .pad(10.)
 }
 
 fn draw_a(ui: &mut Ui) -> Node<Ui> {
