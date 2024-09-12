88c3f1bf5de9c3a00ae672304e41d4392513558d
ejjonny <ejjonny@gmail.com>
2024-09-11 19:27:07 -0600

    fixing constraint bugs

---
+++ src/constraints.rs
@@ -0,0 +1,126 @@
+use crate::models::Size;
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub(crate) struct SizeConstraints {
+    pub(crate) width: Constraint,
+    pub(crate) height: Constraint,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub(crate) struct Constraint {
+    pub(crate) lower: Option<f32>,
+    pub(crate) upper: Option<f32>,
+}
+
+impl Constraint {
+    pub(crate) fn none() -> Self {
+        Self {
+            lower: None,
+            upper: None,
+        }
+    }
+}
+
+impl SizeConstraints {
+    pub(crate) fn combine_adjacent_priority(self, other: Self) -> Self {
+        SizeConstraints {
+            width: self.width.combine_adjacent_priority(other.width),
+            height: self.height.combine_adjacent_priority(other.height),
+        }
+    }
+    pub(crate) fn combine_equal_priority(self, other: Self) -> Self {
+        SizeConstraints {
+            width: self.width.combine_equal_priority(other.width),
+            height: self.height.combine_equal_priority(other.height),
+        }
+    }
+    pub(crate) fn combine_sum(self, other: Self) -> Self {
+        SizeConstraints {
+            width: self.width.combine_sum(other.width),
+            height: self.height.combine_sum(other.height),
+        }
+    }
+}
+
+impl Constraint {
+    pub(crate) fn combine_adjacent_priority(self, other: Self) -> Self {
+        // This always takes the bigger bound
+        let lower = match (self.lower, other.lower) {
+            (None, None) => None,
+            (None, Some(a)) | (Some(a), None) => Some(a),
+            (Some(bound_a), Some(bound_b)) => Some(bound_a.max(bound_b)),
+        };
+        // In terms of upper constraints - no constraint is biggest
+        let upper = match (self.upper, other.upper) {
+            (None, None) => None,
+            (None, Some(_)) | (Some(_), None) => None,
+            (Some(bound_a), Some(bound_b)) => Some(bound_a.max(bound_b)),
+        };
+        Constraint { lower, upper }
+    }
+    pub(crate) fn combine_equal_priority(self, other: Self) -> Self {
+        let lower = match (self.lower, other.lower) {
+            (None, None) => None,
+            (None, Some(a)) | (Some(a), None) => Some(a),
+            (Some(bound_a), Some(bound_b)) => Some(bound_a.max(bound_b)),
+        };
+        let upper = match (self.upper, other.upper) {
+            (None, None) => None,
+            (None, Some(a)) | (Some(a), None) => Some(a),
+            (Some(bound_a), Some(bound_b)) => Some(bound_a.max(bound_b)),
+        };
+        Constraint { lower, upper }
+    }
+    pub(crate) fn combine_sum(self, other: Self) -> Self {
+        let lower = match (self.lower, other.lower) {
+            (None, None) => None,
+            (None, Some(a)) | (Some(a), None) => Some(a),
+            (Some(bound_a), Some(bound_b)) => Some(bound_a.max(bound_b)),
+        };
+        let upper = match (self.upper, other.upper) {
+            (None, None) => None,
+            (None, Some(bound)) | (Some(bound), None) => Some(bound),
+            (Some(bound_a), Some(bound_b)) => Some(bound_a + bound_b),
+        };
+        Constraint { lower, upper }
+    }
+}
+
+impl From<Size> for SizeConstraints {
+    fn from(value: Size) -> Self {
+        SizeConstraints {
+            width: if value.width.is_some() {
+                Constraint {
+                    lower: value.width,
+                    upper: value.width,
+                }
+            } else if value.width_min.is_some() || value.width_max.is_some() {
+                Constraint {
+                    lower: value.width_min,
+                    upper: value.width_max,
+                }
+            } else {
+                Constraint {
+                    lower: None,
+                    upper: None,
+                }
+            },
+            height: if value.height.is_some() {
+                Constraint {
+                    lower: value.height,
+                    upper: value.height,
+                }
+            } else if value.height_min.is_some() || value.height_max.is_some() {
+                Constraint {
+                    lower: value.height_min,
+                    upper: value.height_max,
+                }
+            } else {
+                Constraint {
+                    lower: None,
+                    upper: None,
+                }
+            },
+        }
+    }
+}
