97483717349a6d686049bce5748f9681c66b6346
ejjonny <ejjonny@gmail.com>
2024-09-14 15:37:05 -0600

    scaling

--- examples/demo-site/src/app.rs
+++ examples/demo-site/src/app.rs
@@ -31,6 +31,7 @@ const DEMO_DESTRUCTIVE_SECONDARY: Color32 = Color32::from_rgb(210, 40, 40);
 const DEMO_HINT: Color32 = Color32::from_rgb(35, 35, 38);
 const DEMO_FG: Color32 = Color32::from_rgb(250, 250, 255);
 const DEMO_FG_SECONDARY: Color32 = Color32::from_rgb(180, 180, 183);
+const SCALE: f32 = 0.5;
 
 fn my_layout_fn(ui: &mut Ui) -> Node<Ui> {
     stack(vec![
@@ -53,7 +54,7 @@ fn my_layout_fn(ui: &mut Ui) -> Node<Ui> {
 
 fn footer(ui: &mut Ui) -> Node<Ui> {
     row_spaced(
-        20.,
+        20. * SCALE,
         vec![
             draw_label_color(ui, "Game", 12., DEMO_FG_SECONDARY),
             draw_label_color(ui, "Terms & Conditions", 12., DEMO_FG_SECONDARY),
@@ -67,8 +68,8 @@ fn footer(ui: &mut Ui) -> Node<Ui> {
             ),
         ],
     )
-    .pad(10.)
-    .height(40.)
+    .pad(10. * SCALE)
+    .height(40. * SCALE)
 }
 
 fn main_view(ui: &mut Ui) -> Node<Ui> {
@@ -78,27 +79,27 @@ fn main_view(ui: &mut Ui) -> Node<Ui> {
     stack(vec![stack(vec![
         rect(DEMO_GRAY, DEMO_HINT, 5.),
         column_spaced(
-            10.,
+            10. * SCALE,
             vec![
                 row_spaced(
-                    10.,
+                    10. * SCALE,
                     vec![
                         column_spaced(
-                            10.,
+                            10. * SCALE,
                             vec![
                                 draw_label(ui, "Public profile", 18.),
-                                multiline_label(ui, profile_blurb, 10.).height(50.),
+                                multiline_label(ui, profile_blurb, 10.).height(50. * SCALE),
                             ],
                         )
                         .align(Align::TopLeading)
-                        .width_range(300.0..),
+                        .width_range((300.0 * SCALE)..),
                         column_spaced(
-                            10.,
+                            10. * SCALE,
                             vec![
                                 stack(vec![
                                     rect(DEMO_FG, DEMO_BG, 5.),
                                     row_spaced(
-                                        10.,
+                                        10. * SCALE,
                                         vec![
                                             draw_label_color(
                                                 ui,
@@ -109,16 +110,16 @@ fn main_view(ui: &mut Ui) -> Node<Ui> {
                                             icon(include_image!("../assets/copy.svg")).aspect(1.),
                                         ],
                                     )
-                                    .pad(5.),
+                                    .pad(3. * SCALE),
                                 ])
-                                .height(25.),
+                                .height(25. * SCALE),
                                 row_spaced(
-                                    10.,
+                                    10. * SCALE,
                                     vec![
                                         stack(vec![
                                             rect(DEMO_FG, DEMO_BG, 5.),
                                             row_spaced(
-                                                10.,
+                                                10. * SCALE,
                                                 vec![
                                                     icon(include_image!("../assets/share.svg"))
                                                         .aspect(1.),
@@ -130,13 +131,13 @@ fn main_view(ui: &mut Ui) -> Node<Ui> {
                                                     ),
                                                 ],
                                             )
-                                            .pad(5.),
+                                            .pad(3. * SCALE),
                                         ])
-                                        .height(25.),
+                                        .height(25. * SCALE),
                                         stack(vec![
                                             rect(DEMO_FG, DEMO_BG, 5.),
                                             row_spaced(
-                                                10.,
+                                                10. * SCALE,
                                                 vec![
                                                     icon(include_image!("../assets/map-pin.svg"))
                                                         .aspect(1.),
@@ -148,9 +149,9 @@ fn main_view(ui: &mut Ui) -> Node<Ui> {
                                                     ),
                                                 ],
                                             )
-                                            .pad(5.),
+                                            .pad(3. * SCALE),
                                         ])
-                                        .height(25.),
+                                        .height(25. * SCALE),
                                     ],
                                 ),
                             ],
@@ -160,23 +161,25 @@ fn main_view(ui: &mut Ui) -> Node<Ui> {
                 .y_align(YAlign::Top),
                 col_divider(DEMO_GRAY).height(1.),
                 row_spaced(
-                    10.,
+                    10. * SCALE,
                     vec![
                         column_spaced(
-                            10.,
+                            10. * SCALE,
                             vec![
                                 draw_label(ui, "Edit PFP", 18.),
-                                multiline_label(ui, pic_blurb, 10.).height(50.),
+                                multiline_label(ui, pic_blurb, 10.).height(50. * SCALE),
                             ],
                         )
                         .align(Align::TopLeading)
-                        .width_range(300.0..),
+                        .width_range((300.0 * SCALE)..),
                         row_spaced(
-                            10.,
+                            10. * SCALE,
                             vec![
-                                rect(DEMO_FG, DEMO_BG, 100.).height(30.).width(30.),
+                                rect(DEMO_FG, DEMO_BG, 100.)
+                                    .height(30. * SCALE)
+                                    .width(30. * SCALE),
                                 column_spaced(
-                                    5.,
+                                    5. * SCALE,
                                     vec![
                                         draw_label(ui, "@UserName", 12.),
                                         draw_label_color(
@@ -190,50 +193,51 @@ fn main_view(ui: &mut Ui) -> Node<Ui> {
                                 .x_align(XAlign::Leading),
                                 stack(vec![
                                     rect(DEMO_FG, DEMO_BG, 5.),
-                                    draw_label_color(ui, "Upload", 12., DEMO_FG_SECONDARY).pad(5.),
+                                    draw_label_color(ui, "Upload", 12., DEMO_FG_SECONDARY)
+                                        .pad(5. * SCALE),
                                 ])
-                                .height(25.),
+                                .height(25. * SCALE),
                                 stack(vec![
                                     rect(DEMO_DESTRUCTIVE_SECONDARY, DEMO_BG, 5.),
-                                    draw_label_color(ui, "Remove", 12., DEMO_DESTRUCTIVE).pad(5.),
+                                    draw_label_color(ui, "Remove", 12., DEMO_DESTRUCTIVE)
+                                        .pad(5. * SCALE),
                                 ])
-                                .height(25.),
+                                .height(25. * SCALE),
                             ],
                         ),
                     ],
                 ),
                 col_divider(DEMO_GRAY).height(1.),
                 row_spaced(
-                    10.,
+                    10. * SCALE,
                     vec![
                         column_spaced(
-                            10.,
+                            10. * SCALE,
                             vec![
                                 draw_label(ui, "Edit personal information", 18.),
-                                multiline_label(ui, info_blurb, 10.).height(50.),
+                                multiline_label(ui, info_blurb, 10.).height(50. * SCALE),
                             ],
                         )
                         .align(Align::TopLeading)
-                        .width_range(300.0..),
+                        .width_range((300.0 * SCALE)..),
                         column_spaced(
-                            5.,
+                            5. * SCALE,
                             vec![
                                 draw_label_color(ui, "Edit username", 12., DEMO_FG_SECONDARY),
                                 stack(vec![
                                     rect(DEMO_FG, DEMO_BG, 5.),
                                     draw_label_color(ui, "@UserName", 12., DEMO_FG)
-                                        .x_align(XAlign::Leading)
-                                        .pad(5.),
+                                        .x_align(XAlign::Leading),
                                 ])
-                                .height(25.),
+                                .height(25. * SCALE),
                                 draw_label_color(ui, "Bio", 12., DEMO_FG_SECONDARY),
                                 stack(vec![
                                     rect(DEMO_FG, DEMO_BG, 5.),
                                     draw_label_color(ui, "Living, laughing, loving", 12., DEMO_FG)
                                         .align(Align::TopLeading)
-                                        .pad(5.),
+                                        .pad(5. * SCALE),
                                 ])
-                                .height(50.),
+                                .height(50. * SCALE),
                             ],
                         )
                         .x_align(XAlign::Leading),
@@ -243,28 +247,28 @@ fn main_view(ui: &mut Ui) -> Node<Ui> {
             ],
         )
         .align(Align::TopLeading)
-        .pad_y(40.)
-        .pad_x(30.),
+        .pad_y(40. * SCALE)
+        .pad_x(30. * SCALE),
         rect_stroke(DEMO_GRAY),
     ])
-    .pad(20.)])
+    .pad(20. * SCALE)])
 }
 
 fn side_bar(ui: &mut Ui) -> Node<Ui> {
     column_spaced(
-        20.,
+        20. * SCALE,
         vec![
-            draw_label(ui, "BACKER", 22.).height(30.),
-            col_divider(DEMO_GRAY).pad_x(-30.).height(1.),
+            draw_label(ui, "BACKER", 22.).height(30. * SCALE),
+            col_divider(DEMO_GRAY).pad_x(-30. * SCALE).height(1.),
             draw_label(ui, "Home", 10.),
             draw_label(ui, "Explore", 10.),
             draw_label(ui, "Marketplace", 10.),
             draw_label(ui, "My Account", 10.),
-            col_divider(DEMO_GRAY).pad_trailing(-20.).height(1.),
+            col_divider(DEMO_GRAY).pad_trailing(-20. * SCALE).height(1.),
             draw_label(ui, "Activity", 10.),
             draw_label(ui, "News", 10.),
             draw_label(ui, "Docs", 10.),
-            col_divider(DEMO_GRAY).pad_trailing(-20.).height(1.),
+            col_divider(DEMO_GRAY).pad_trailing(-20. * SCALE).height(1.),
             draw_label(ui, "Twitter", 10.),
             draw_label(ui, "Telegram", 10.),
             draw_label(ui, "Medium", 10.),
@@ -272,33 +276,33 @@ fn side_bar(ui: &mut Ui) -> Node<Ui> {
         ],
     )
     .align(Align::TopLeading)
-    .pad(30.)
-    .width(200.)
+    .pad(30. * SCALE)
+    .width(200. * SCALE)
 }
 
 fn header(ui: &mut Ui) -> Node<Ui> {
     row_spaced(
-        10.,
+        10. * SCALE,
         vec![
             draw_label(ui, "My Account", 18.)
                 .y_align(YAlign::Bottom)
-                .width(100.),
+                .width(110. * SCALE),
             space(),
             stack(vec![
                 rect(DEMO_FG, DEMO_HINT, 5.),
                 draw_label(ui, "$115,000", 12.),
             ])
-            .width(80.),
+            .width(80. * SCALE),
             stack(vec![
                 rect(DEMO_FG, DEMO_HINT, 5.),
                 row(vec![draw_label(ui, "Operational", 12.)]),
             ])
-            .width(90.),
+            .width(90. * SCALE),
             stack(vec![
                 rect(DEMO_FG, DEMO_HINT, 5.),
                 icon(include_image!("../assets/bell.svg"))
                     .aspect(1.)
-                    .pad_y(8.5)
+                    .pad_y(8.5 * SCALE)
                     .aspect(1.),
             ])
             .aspect(1.),
@@ -306,16 +310,16 @@ fn header(ui: &mut Ui) -> Node<Ui> {
                 rect(DEMO_FG, DEMO_HINT, 5.),
                 icon(include_image!("../assets/user.svg"))
                     .aspect(1.)
-                    .pad_y(8.5)
+                    .pad_y(8.5 * SCALE)
                     .aspect(1.),
             ])
             .aspect(1.),
         ],
     )
-    .pad_top(35.)
-    .pad_bottom(15.)
-    .pad_x(30.)
-    .height(80.)
+    .pad_top(35. * SCALE)
+    .pad_bottom(15. * SCALE)
+    .pad_x(30. * SCALE)
+    .height(80. * SCALE)
 }
 
 fn icon(image: impl Into<ImageSource<'static>> + 'static) -> Node<Ui> {
@@ -330,9 +334,9 @@ fn multiline_label<S: AsRef<str> + 'static>(_ui: &'_ mut Ui, text: S, size: f32)
         let layout_job = LayoutJob::single_section(
             text.as_ref().to_string(),
             egui::TextFormat {
-                font_id: egui::FontId::new(size, egui::FontFamily::Proportional),
+                font_id: egui::FontId::new(size * SCALE, egui::FontFamily::Proportional),
                 extra_letter_spacing: 0.,
-                line_height: Some(14.),
+                line_height: Some(14. * SCALE),
                 color: Color32::WHITE,
                 background: Color32::TRANSPARENT,
                 italics: false,
@@ -362,7 +366,7 @@ fn draw_label_color<S: AsRef<str> + 'static>(
 ) -> Node<Ui> {
     let job = LayoutJob::simple(
         text.as_ref().to_string(),
-        egui::FontId::new(size, egui::FontFamily::Proportional),
+        egui::FontId::new(size * SCALE, egui::FontFamily::Proportional),
         color,
         200.,
     );
@@ -374,7 +378,7 @@ fn draw_label_color<S: AsRef<str> + 'static>(
             rect_from(area),
             egui::Label::new(LayoutJob::simple(
                 text.as_ref().to_string(),
-                egui::FontId::new(size, egui::FontFamily::Proportional),
+                egui::FontId::new(size * SCALE, egui::FontFamily::Proportional),
                 color,
                 200.,
             )),
