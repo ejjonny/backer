3c8f478112f0de054269563c27a2a9bd50b3b341
ejjonny <ejjonny@gmail.com>
2024-10-15 23:09:03 -0600

    docs

--- src/traits/scopable_option.rs
+++ src/traits/scopable_option.rs
@@ -1,26 +1,28 @@
 use super::Scopable;
 
-/// Implement `ScopableOption` to enable usage with [`Node::scope`] for optional state.
+/// Implement `ScopableOption` to enable usage with [`scope`] for an optional subset of your state.
 /// For non-optional state, implement [`Scopable`].
+///
+/// See [`Scopable`] for more example code.
+///
+/// ```rust
+/// use backer::traits::ScopableOption;
+///
+/// struct A {
+///     b: Option<B>,
+/// }
+///
+/// struct B;
+///
+/// struct Scoper;
+/// impl ScopableOption<A, B> for Scoper {
+///     fn scope_option<Result>(scoping: &mut A, f: impl FnOnce(Option<&mut B>) -> Result) -> Result {
+///        f(scoping.b.as_mut())
+///     }
+/// }
+/// ```
 pub trait ScopableOption<Scoping, Scoped> {
     /// Provide a scoped mutable reference to an optional subset of your state.
-    ///
-    /// ```rust
-    /// use backer::traits::ScopableOption;
-    ///
-    /// struct A {
-    ///     b: Option<B>,
-    /// }
-    ///
-    /// struct B;
-    ///
-    /// struct Scoper;
-    /// impl ScopableOption<A, B> for Scoper {
-    ///     fn scope_option<Result>(scoping: &mut A, f: impl FnOnce(Option<&mut B>) -> Result) -> Result {
-    ///        f(scoping.b.as_mut())
-    ///     }
-    /// }
-    /// ```
     fn scope_option<Result>(
         scoping: &mut Scoping,
         f: impl FnOnce(Option<&mut Scoped>) -> Result,
