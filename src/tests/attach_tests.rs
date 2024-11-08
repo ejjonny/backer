108cf21f8a02e68caaed3a582e39018e13fa5fad
ejjonny <ejjonny@gmail.com>
2024-11-07 21:36:15 -0700

    constraint & align consistency

---
+++ src/tests/attach_tests.rs
@@ -0,0 +1,36 @@
+#[cfg(test)]
+mod tests {
+    use crate::layout::*;
+    use crate::models::*;
+    use crate::nodes::*;
+    #[test]
+    fn test_attach() {
+        Layout::new(|()| {
+            stack(vec![
+                //>
+                draw(|a, _| {
+                    assert_eq!(a, Area::new(40., 40., 20., 20.));
+                })
+                .width(20.)
+                .height(20.)
+                .pad(10.)
+                .attach_under(draw(|a, _| {
+                    assert_eq!(a, Area::new(30., 30., 40., 40.));
+                })),
+            ])
+        })
+        .draw(Area::new(0., 0., 100., 100.), &mut ());
+        Layout::new(|()| {
+            stack(vec![draw(|a, _| {
+                assert_eq!(a, Area::new(40., 40., 20., 20.));
+            })
+            .width(20.)
+            .height(20.)
+            .pad(10.)
+            .attach_over(draw(|a, _| {
+                assert_eq!(a, Area::new(30., 30., 40., 40.));
+            }))])
+        })
+        .draw(Area::new(0., 0., 100., 100.), &mut ());
+    }
+}
